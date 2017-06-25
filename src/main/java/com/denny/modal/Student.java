2de0ed7ca8ce09d29dc8478d3d9de62c231f106d
denny.wang <992673023@qq.com>
2017-06-25 14:39:44 +0800

    student

---
+++ src/main/java/com/denny/modal/Student.java
@@ -0,0 +1,16 @@
+package com.denny.modal;
+
+/**
+ * Created by denny.wang on 2017/6/25.
+ */
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
