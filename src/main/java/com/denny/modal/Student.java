ef27ce91461546e253aee1c072fbd3562bb9eac2
denny.wang <992673023@qq.com>
2017-06-25 15:21:57 +0800

    student

--- src/main/java/com/denny/modal/Student.java
+++ src/main/java/com/denny/modal/Student.java
@@ -6,6 +6,36 @@ package com.denny.modal;
 public class Student {
     private String name;
 
+    private String sex;
+
+    private int age;
+
+    private String number;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
     public String getName() {
         return name;
     }
